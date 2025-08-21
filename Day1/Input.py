925c184d63b233c01004546e46748c4c621433d0
Vicky Kumar <connect.vickykumar@gmail.com>
2025-08-21 18:13:53 +0530

    learned about taking input

---
+++ Day1/Input.py
@@ -0,0 +1,15 @@
+# what is your name:
+# vicky
+# hello vicky!
+
+# whenever you put the input that is vicky here
+# the entire input replaced with the input user provide 
+
+# like input("your name ")  after input it just become the you typed
+#  the moment it get the input it get disapear and there is no way 
+# to use the input in future for other purpose
+
+# AND HERE COMES VARIABLE TO STORE THAT FOR FUTURE
+
+
+print("hello "+input("what is your name:\n")+" !")
