43c66d78893c16fdd214f31c0f39bec9f91c293f
Vicky Kumar <connect.vickykumar@gmail.com>
2025-08-21 18:16:28 +0530

    learned string maipulation/concatenation

---
+++ Day1/StringManipulation.py
@@ -0,0 +1,9 @@
+# \n to add a new line 
+print("hello world\nhello world")
+
+# add a + sign to combine two string in print function 
+# it is called string concatenation 
+
+print("Hello" + "World")  # gives HelloWorld
+print("Hello" + " World")  # to get Hello World add a space character
+print("Hello"+" "+"World")  # another way (simply add a spce as string)
