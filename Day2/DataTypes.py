2f55b2772c69c5ce456d33250b1e1c5d45dd0117
Vicky Kumar <connect.vickykumar@gmail.com>
2025-08-22 09:40:19 +0530

    Learned about Data types

---
+++ Day2/DataTypes.py
@@ -0,0 +1,18 @@
+print("Hello"[4])    # give 'o' as an output
+print("Hello"[-4])   #give fourth from last viz. e
+
+# string of Anything : anything inside "" is string no matter what
+print("123"+"456")  #give 123456
+print(12+12)        #give 24 because it's not a string
+
+# large integer
+print("123_456_789")  #123456789
+
+# floatintg point number
+print(3.56)
+
+#boolean data types
+
+#True
+#False
+
