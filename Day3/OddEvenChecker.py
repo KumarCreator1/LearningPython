19b9835ec51d7beba6d89110d2a84b9caf0fed5f
Vicky Kumar <connect.vickykumar@gmail.com>
2025-08-22 14:48:53 +0530

    made an odd even checker

---
+++ Day3/OddEvenChecker.py
@@ -0,0 +1,8 @@
+# check an integer is odd or even
+num = int(input("enter the number: "))
+
+# check the condition for odd even
+if num%2 == 0 :
+    print(f"{num} is an even number")
+else:
+    print(f"{num} is not an even number")
