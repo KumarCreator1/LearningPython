348fc449d3521d474b8adb03e366c393c0f52436
Vicky Kumar <connect.vickykumar@gmail.com>
2025-08-22 14:01:15 +0530

    mathematical operation in python

---
+++ Day2/MathOperation.py
@@ -0,0 +1,29 @@
+print(123+456)
+print(7-3)
+
+print(6/3)  #give 2.0 not 2
+print(type(6/3)) #float
+print(6//3) #this will give 2
+print(5/3)  #give 1.6666666667
+print(5//3) #give 1  something like demotion
+
+print(2*3)   #multiply = 6
+print(2**3)  #exponentiation 2 raised power 3  = 8
+
+# if multiple operators used than calculation will follow a PRIORITY
+#PEMDAS_LR (left to right or say Left precidence/associativity)
+# ()
+# **
+# * or /
+# + or-
+
+print(3*3+3/3-3) # 3*3+1.0-3 ->  # 9+1.0-3   -> # 10-3.0= 7.0 
+
+print(3*(3+3)/3-3)
+ # 3*(3+3) /3-3 solve P
+# 3*6/3-3  solve 3*6 due to left associativity
+#18/3-3
+# 6.0-3  =  3.0
+
+ 
+
