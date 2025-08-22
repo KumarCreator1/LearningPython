14050c310e573d4a17f9ba1125780df870d22a8e
Vicky Kumar <connect.vickykumar@gmail.com>
2025-08-22 14:02:41 +0530

    use of f string in python

---
+++ Day2/fString.py
@@ -0,0 +1,8 @@
+score = 0
+height = 0
+is_winning = True
+# using an f string you can use multiple data type after a string 
+# no comma like C is needed 
+# remember to {}
+# the {variable} should be inside the double code of string itself
+print(f"Your score is = {score} And your height is = {height} and your winning is {is_winning}") 
