3897fe62bf6678ecf201dadfb09fcc090d23d225
Vicky Kumar <connect.vickykumar@gmail.com>
2025-08-22 09:43:03 +0530

    learned Type Casting/Data Type conversion

---
+++ Day2/TypeCasting.py
@@ -0,0 +1,17 @@
+print(type("Hello"))    #<class 'str'>
+print(type(123))        #<class 'int'>
+print(type(5.68))       #<class 'float'>
+print(type(True))       #<class 'bool'>
+
+#type coversion/ type Casting
+print("123"+"456") #123456
+#type cast a string to an int
+print(int("123")+int("456")) #579
+
+#int()
+#float()
+#str()
+#bool()
+
+print("The no. of letter in your name is:"+ str(len(input("What's your name? "))))
+
