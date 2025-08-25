ad4564d0c5ed7fe825f56a857f80aea426e775f0
Vicky Kumar <connect.vickykumar@gmail.com>
2025-08-25 20:06:29 +0530

    using range function in for loop

---
+++ Day5/RangeinForLoop.py
@@ -0,0 +1,10 @@
+
+
+for number in range(1,10):
+    print(number)
+
+# sum first 100 naturals
+sum=0
+for number in range(1,101,1): # start from 1 till 100 step up each time by 1
+    sum+= number
+print(sum) # 5050
