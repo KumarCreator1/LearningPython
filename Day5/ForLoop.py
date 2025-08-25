7b09fa1c36bde7ce451db92ef1751f55310991d6
Vicky Kumar <connect.vickykumar@gmail.com>
2025-08-25 20:02:01 +0530

    learned about for loop in python

---
+++ Day5/ForLoop.py
@@ -0,0 +1,10 @@
+basket = ["apple","pear","peach"]
+
+#fruit is a temp varible which will hold the item from list "basket"
+# it will be updated each time for oop runs 
+for fruit in basket:     # loop will execute three time
+    print(fruit)         #print the item holded by variable
+    print(fruit + " pie") 
+    print(basket)        # print the list # will be printed three times
+ 
+
