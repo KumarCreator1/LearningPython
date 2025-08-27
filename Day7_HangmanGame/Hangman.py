83490d792f4c140c1b2efc223bb912830fd2fe13
Vicky Kumar <connect.vickykumar@gmail.com>
2025-08-27 10:22:50 +0530

    succefully implemented Loop for Guessing again

--- Day7_HangmanGame/Hangman.py
+++ Day7_HangmanGame/Hangman.py
@@ -1,33 +1,32 @@
 import random
-
 word_list = ["python", "guitar", "planet", "tiger", "castle"]
 
-# choose a random word from list
 hide_word = random.choice(word_list)
+
 print(hide_word)
+gameover = False
+correct_li= []
 
-
-# : display blanks in place of word
 NumberOfBlanks = len(hide_word)
 blank_list = []
 blank_str =""
 for i in range(0,NumberOfBlanks):
-    blank_list.append("_ ")
-    blank_str+= "_ "
+    blank_list.append("_")
+    blank_str+= "_"
 
-# print(blank_list)
-print(blank_str)
 
-# ask the user to guess a latter and maake it lowerr case
-guess = input("Guess a letter: ").lower()
-# NumberOfLives = NumberOfBlanks
+while gameover != True:
+    display = ""
+    guess = input("Guess a letter: ").lower()
+    for alph in hide_word:
+        if alph == guess:
+            display+= guess
+            correct_li.append(guess)
+        elif alph in correct_li:
+            display+= alph
+        else:
+            display += "_"
+    print(display)
 
-# place the rightly gussed word in right Placeholder
-display = ""
-for alph in hide_word:
-    if alph == guess:
-        display+= guess
-    else:
-        display += "_ "
-        # NumberOfLives-= 1
-print(display)
+    if "_" not in display:
+        gameover = True
