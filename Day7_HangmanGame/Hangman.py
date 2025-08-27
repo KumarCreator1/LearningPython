07f828799f2a3a7d6281315a84c687ba7ef25477
Vicky Kumar <connect.vickykumar@gmail.com>
2025-08-27 09:28:35 +0530

    setting the core of Game

---
+++ Day7_HangmanGame/Hangman.py
@@ -0,0 +1,23 @@
+import random
+
+word_list = ["python", "guitar", "planet", "tiger", "castle"]
+
+# choose a random word from list
+hide_word = random.choice(word_list)
+print(hide_word)
+
+
+# : display blanks in place of word
+NumberOfBlanks = len(hide_word)
+blank_list = []
+blank_str =""
+for i in range(0,NumberOfBlanks):
+    blank_list.append("_ ")
+    blank_str+= "_ "
+
+# print(blank_list)
+print(blank_str)
+
+# ask the user to guess a latter and maake it lowerr case
+guess = input("Guess a letter: ").lower()
+# NumberOfLives = NumberOfBlanks
