2ac9e93dd3bae05aba3dcce2c2631ef36c367b68
Vicky Kumar <connect.vickykumar@gmail.com>
2025-08-28 18:21:48 +0530

    learned about funtion with input

---
+++ Day8/FunctionwInput.py
@@ -0,0 +1,7 @@
+def my_function(name,location): #function that allow inputs
+    print(f"Hello {name}")
+    print(f"your location is {location}")
+
+my_function("Vicky kumar","delhi") #calling function with vicky kumar as input in name variable
+
+
