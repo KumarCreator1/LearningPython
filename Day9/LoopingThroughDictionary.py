4f80c3c3ff9621331cb7cc9b8e25316f0bc5d0be
Vicky Kumar <connect.vickykumar@gmail.com>
2025-09-07 09:45:34 +0530

    learned about looping through dictionary

---
+++ Day9/LoopingThroughDictionary.py
@@ -0,0 +1,12 @@
+programing_dictionary = {
+    "Bug":"An error in program",
+    "Function": "A pies of code that you can call and call over again",
+    "Loop": "The action that ocurr again and agian",
+    123:"234",
+    234:456,
+}
+
+for key in programing_dictionary:
+    print(key)
+    print(f"{programing_dictionary[key]}\n")
+    
