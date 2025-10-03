a8c883aa714c237acca0c04a23beb9bee54b31b9
Vicky Kumar <connect.vickykumar@gmail.com>
2025-10-03 20:13:30 +0530

    Learned about return value in function

---
+++ Day10/FunctionWithOutput.py
@@ -0,0 +1,12 @@
+# fuction with output
+def myfunction():
+    return 4*3  # output which can be stored in any variable
+
+result = myfunction() #returned output is stored in result contianer
+print(result)
+
+
+def name(fname,lname): 
+    return (fname+" "+lname).title()
+
+print(name("vIcKY","kUmAr"))
